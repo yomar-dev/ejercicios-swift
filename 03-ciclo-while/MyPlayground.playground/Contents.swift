77b77c5f73db9840c3f44a728ef43fc2a787f93f
Yomar Miranda <mirandayomar@gmail.com>
2018-04-02 16:26:49 -0500

    03 - Ejemplo ciclo While.

---
+++ 03-ciclo-while/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+// Contador
+var i = 1
+
+while i <= 10{
+    print(i)
+    i += 1
+}
+
+
+var j = 1
+
+repeat{
+    print(j)
+    j += 1
+}while j <= 10
